b4594abfafb9c1f0202b93c485684ebab9ecd6b0
Nova8 <408004735@qq.com>
2022-01-12 17:55:47 +0800

    0112

--- Python/Code/飞机大战/AirPlaneFighting.py
+++ Python/Code/飞机大战/AirPlaneFighting.py
@@ -1,6 +1,7 @@
 import pygame
-import time
+from plane_sprites import *
 
+# 开始前必须调用
 pygame.init()
 # 创建游戏主窗口
 screen = pygame.display.set_mode((480, 700))
@@ -13,15 +14,33 @@ screen.blit(bg, (0, 0))
 hero = pygame.image.load("./images/me1.png")
 screen.blit(hero, (200, 500))
 
-# 更新屏幕
-pygame.display.update()
-
 # 创建时钟对象
 clock = pygame.time.Clock()
 
-i = 0
+hero_rect = pygame.Rect(200, 500, 102, 126)
+
+# 创建敌机精灵
+enemy = GameSprite("./images/enemy1.png")
+enemy1 = GameSprite("./images/enemy1.png",3)
+enemy_group = pygame.sprite.Group(enemy,enemy1)
+
 while True:
-    clock.tick(1)
-    print(i)
-    i = i + 1
+    clock.tick(60)  # 设置帧率
+    # 监听用户事件
+    for event in pygame.event.get():
+        # 退出游戏
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    hero_rect.y -= 1
+    screen.blit(bg, (0, 0))  # 重新绘制背景图像，不然会有飞机的残影
+    screen.blit(hero, hero_rect)
+
+    enemy_group.update()
+    enemy_group.draw(screen)
+
+    pygame.display.update()
+
+# 退出前必须调用
 pygame.quit()
---
+++ Python/Code/飞机大战/plane_main.py
@@ -0,0 +1,3 @@
+import pygame
+from plane_sprites import *
+
